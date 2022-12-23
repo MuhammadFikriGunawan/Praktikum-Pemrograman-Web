86573bd15de6bc95171e5cda928489444f9f8ae6
MuhammadFikriGunawan <121302758+MuhammadFikriGunawan@users.noreply.github.com>
2022-12-23 21:52:06 +0700

    Add files via upload
    Laporan Akhir Praktikum Pemweb Muhammad Fikri Gunawan - 2170231081 - A1 Reguler Pagi

---
+++ Muhammad fikri gunawan - 2170231081/Pertemuan 4/latihan/latihan2.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title> Pemrograman PHP dengan Array</title>
+</head>
+<body>
+<?php
+
+$nama[] = "Muhammad";
+$nama[] = "Fikri";
+$nama[] = "Gunawan";
+echo $nama[1] . $nama[2] . $nama[0];
+echo "<br>";
+?>
+</body>
+</html>
